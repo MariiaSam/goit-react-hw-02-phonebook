575db282147c90d0ca0b4828240ac1640228f712
MariiaSam <kontragent@ukr.net>
2023-12-02 21:08:36 +0200

    corr

--- src/components/Form/Form.jsx
+++ src/components/Form/Form.jsx
@@ -1,4 +1,4 @@
-import PropTypes from 'prop-types';
+// import PropTypes from 'prop-types';
 import { Formik } from 'formik';
 import { object, string } from 'yup';
 import { nanoid } from 'nanoid';
